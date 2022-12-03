fe4ade425bacf5f68cda0a95a9578704cd628a1e
ibaliyan <ibaliyan@uwaterloo.ca>
2022-12-03 16:29:58 -0500

    Fix errors in boardmodel.h/cc

--- boardmodel.cc
+++ boardmodel.cc
@@ -1,52 +1,78 @@
 #include "boardmodel.h"
 #include "piece.h"
 
+explicit BoardModel::BoardModel(Piece* boardIn[8][8]){
+    // Must copy board over manually, since cannot use MIL with two-dimensional array
+    // in current C++ version
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            myBoard[i][j] = boardIn[i][j];
+        }
+    }
+}
+
 void BoardModel::display(){
     notifyObservers();
 }
 
 
-BoardModel::~Board(){
-    delete board;
+BoardModel::~BoardModel(){
+    delete myBoard;
 }
 
 Piece* BoardModel::getState(int x, int y) const {
-    return board[x][y];
+    return myBoard[x][y];
 }
 
 
 bool BoardModel::isCheck(){
-    //TODO: [INSERT CODE]
+    // TODO: [INSERT CODE]
 }
 
 
 bool BoardModel::isCheckmate(){
-    //TODO: [INSERT CODE]
+    // TODO: [INSERT CODE]
 }
 
 
 bool BoardModel::isStalemate(){
-    //TODO: [INSERT CODE]
+    // TODO: [INSERT CODE]
 }
 
 
 void BoardModel::addPiece(Piece* piece, int x, int y){
-    //TODO: [INSERT CODE]
+    // If myBoard[x][y] is not a nullptr, delete the piece currently there
+    if(myBoard[x][y]){
+        delete myBoard[x][y];
+    }
+    myBoard[x][y] = piece;
 }
 
 void BoardModel::removePiece(Piece* piece, int x, int y){
-    //TODO: [INSERT CODE]
+    delete myBoard[x][y];
 }
 
 void BoardModel::undo(){
-    //TODO: [INSERT CODE]
+    // TODO: [INSERT CODE]
 }
 
 void BoardModel::undo(string specialMove){
-    //TODO: [INSERT CODE]
+    // TODO: [INSERT CODE]
 }
 
-void BoardModel::changeColour(string colour){
-    //TODO: [INSERT CODE]
+void BoardModel::changeTurn(string colour){
+    turn = colour;
 }
 
+string BoardModel::getTurn() const {
+    return turn;
+}
+
+void BoardModel::nextTurn() {
+    // next turn
+    if(turn == "black"){
+      turn = "white";
+    } else{
+      turn = "black";
+    }
+}
--- boardmodel.h
+++ boardmodel.h
@@ -6,17 +6,27 @@
 #include <string>
 #include "subject.h"
 
+using namespace std;
+
 class Piece;
 
 class BoardModel : public Subject {
-  Piece* board[8][8];
+  Piece*** myBoard; // Really means Piece[][], two-dimensional array
+  string turn;
 
+  // Variables to undo last move
+  Piece* lastCapturedPiece;
+  Piece* lastActionPiece;
+  int lastActionX;
+  int lastActionY;
+  
  public:
-  explicit Board(Piece* board[8][8]): board{board} {}
+  explicit BoardModel(Piece* boardIn[8][8]);
 
-  Piece*[][] *&board() { return board; }
+  // Returns Piece*[][], but formatted as Piece*** for C++ constraints
+  Piece*** &board() { return myBoard; }
   void display(); // Notify observers, which display to user
-  ~Board();
+  ~BoardModel();
   Piece* getState(int x, int y) const override; // Piece at given x and y position on board
 
   bool isCheck();
@@ -26,7 +36,9 @@ class BoardModel : public Subject {
   void removePiece(Piece* piece, int x, int y);
   void undo();
   void undo(string specialMove);
-  void changeColour(string colour);
+  void changeTurn(string colour);
+  string getTurn() const;
+  void nextTurn(); // Forward to next black/white player's turn
 };
 
 #endif
