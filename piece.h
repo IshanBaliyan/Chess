e323ef588a67bdcbb4a7eea83d0ed8ae40a11aa9
c4melans <c4melans@uwaterloo.ca>
2022-12-05 19:39:50 -0500

    updated piece.h

--- piece.h
+++ piece.h
@@ -1,20 +1,21 @@
-#ifndef PIECE_H
-#define PIECE_H
+#ifndef _PIECE_H_
+#define _PIECE_H_
 #include <string>
 
-class Board{} // forward declaration
+class Board{}; // forward declaration
 
 class Piece {
+        virtual bool canMove(int newX, int newY) = 0;
     public:
         virtual ~Piece();
-        virtual void makeMove(Piece& lastCapturedPiece, Piece*& lastActionPiece, int& lastActionX, int& lastActionY, int newX, int newY) = 0;
-        virtual void makeMove(string replacePiece, Piece& lastCapturedPiece, Piece*& lastActionPiece, int& lastActionX, int& lastActionY, int newX, int newY) = 0;
-        virtual bool willNextMoveCauseCheck(int newX, int newY) = 0;
+        virtual void makeMove(Piece *&lastCapturedPiece, Piece *&lastActionPiece, int &lastActionX, int &lastActionY, int newX, int newY) = 0;
+        virtual void makeMove(std::string replacePiece, Piece *&lastCapturedPiece, Piece *&lastActionPiece, int &lastActionX, int &lastActionY, int newX, int newY) = 0;
         virtual bool willNextMoveStopCurrentCheck(int newX, int newY) = 0;
         virtual string getColour() const = 0;
         virtual int getX() const = 0;
         virtual int getY() const = 0;
-        virtual Board* getBoard() const = 0;
-        virtual string getName() const = 0;
-}
+        virtual BoardModel *getBoard() const = 0;
+        virtual std::string getName() const = 0;
+};
 
+#endif
